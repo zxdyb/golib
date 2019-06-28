bfde7c53f777c1eaa22c6988b0f48762458cbbf8
bin.yin <bin.yin@hwcor.com>
2019-06-28 16:30:41 +0800

    增加锁工厂方法，封装了连接对象，便于直接使用锁对象。

--- redislock.go
+++ redislock.go
@@ -1,14 +1,15 @@
 /**
 ***基于单节点redis 分布式锁
 **/
-package redislock
+package lockercommon
 
 import (
 	"crypto/rand"
 	"encoding/base64"
 	"errors"
-
+	"fmt"
 	"github.com/garyburd/redigo/redis"
+	"time"
 )
 
 type RedisLock struct {
@@ -24,7 +25,7 @@ else
 	return 0
 end`)
 
-func (this *RedisLock) Lock(rd *redis.Conn, timeout int) error {
+func (rdl *RedisLock) Lock(rd redis.Conn, timeout int) error {
 
 	{ //随机数
 		b := make([]byte, 16)
@@ -32,9 +33,9 @@ func (this *RedisLock) Lock(rd *redis.Conn, timeout int) error {
 		if err != nil {
 			return err
 		}
-		this.value = base64.StdEncoding.EncodeToString(b)
+		rdl.value = base64.StdEncoding.EncodeToString(b)
 	}
-	lockReply, err := (*rd).Do("SET", this.lockKey, this.value, "ex", timeout, "nx")
+	lockReply, err := rd.Do("SET", rdl.lockKey, rdl.value, "ex", timeout, "nx")
 	if err != nil {
 		return errors.New("redis fail")
 	}
@@ -45,7 +46,57 @@ func (this *RedisLock) Lock(rd *redis.Conn, timeout int) error {
 	}
 }
 
-func (this *RedisLock) Unlock(rd *redis.Conn) {
-	delScript.Do(*rd, this.lockKey, this.value)
+func (rdl *RedisLock) Unlock(rd redis.Conn) error{
+	_, err := delScript.Do(rd, rdl.lockKey, rdl.value)
+	return err
 }
+
+//定义工厂类，转为创建锁对象，工厂类中持有连接池，创建的锁对象中包含有连接对象
+//注意这里每个连接对象包含了不同的连接对象，使用完成后要记得调用包装对象的Free方法释放连接
+type RedisLockFactory struct {
+	redispool *redis.Pool
+}
+
+func (rdlft *RedisLockFactory) Init(ip string, port int){
+	rdlft.redispool = &redis.Pool{
+		MaxIdle:     10,
+		IdleTimeout: 300 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			tcp := fmt.Sprintf("%s:%d", ip, port)
+			c, err := redis.Dial("tcp", tcp)
+			if err != nil {
+				return nil, err
+			}
+			//fmt.Println("connect redis success!")
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+func (rdlft *RedisLockFactory) CreateRedisLock(key string) *RedisLockWrapper{
+	rdd := rdlft.redispool.Get()
+	return &RedisLockWrapper{rdd, RedisLock{lockKey: key}}
+}
+
+//锁对象包装器，将具体连接对象封装在其中，便于外部直接使用锁对象，而无需关系具体连接
+type RedisLockWrapper struct{
+	rd redis.Conn
+	RedisLock
+}
+func (rdlwp *RedisLockWrapper) Free() error{
+	return rdlwp.rd.Close()
+}
+
+func (rdlwp *RedisLockWrapper) LockEx(timeout int) error {
+	return rdlwp.Lock(rdlwp.rd, timeout)
+}
+
+func (rdlwp *RedisLockWrapper) UnlockEx() error {
+	return rdlwp.Unlock(rdlwp.rd)
+}
+
 
--- redislock_test.go
+++ redislock_test.go
@@ -1,59 +1,91 @@
-package redislock
+package lockercommon
 
 import (
-	"fmt"
-	"testing"
-	"time"
+    "fmt"
+    "testing"
+    "time"
 
-	"github.com/garyburd/redigo/redis"
+    "github.com/garyburd/redigo/redis"
 )
 
+func TestLockWrapper(t *testing.T){
+    rdft := RedisLockFactory{}
+    rdft.Init("47.91.178.4", 9379)
+    key := "yyy"
+    go func() {
+        Alock := rdft.CreateRedisLock(key)
+        err := Alock.LockEx(5)
+        time.Sleep(7 * time.Second)
+        fmt.Println("111", err)
+        err = Alock.UnlockEx() //想删除的是Alock锁，但是Alock 已经被自动删除 ,Block由于value 不一样，所以也不会删除
+        fmt.Println("11111", err) //这里一般情况下不会报错，因为函数内部是通过发送脚本来实现的。
+        defer Alock.Free()
+    }()
+
+    time.Sleep(6 * time.Second) //此时Alock 已经被删除
+    Block := rdft.CreateRedisLock(key)
+    err := Block.LockEx(5) //此时 会获取新的lock Block
+    fmt.Println("222", err)
+    defer Block.Free()
+
+    time.Sleep(2 * time.Second)
+    Clock := rdft.CreateRedisLock(key)
+    err = Clock.LockEx(5) //想获取新的lock Clock，但由于 Block还存在，返回错误
+    fmt.Println("333", err)
+    defer Clock.Free()
+
+    time.Sleep(2 * time.Second)
+
+    fmt.Println("***********************************")
+}
+
 func TestLock(t *testing.T) {
-	rd := Redispool.Get()
-	defer rd.Close()
+    rd := Redispool.Get()
+    defer rd.Close()
 
-	go func() {
-		Alock := RedisLock{lockKey: "xxxxx"}
-		err := Alock.Lock(&rd, 5) //5 秒后自动删除Alock
+    go func() {
+        Alock := RedisLock{lockKey: "xxxxx"}
+        err := Alock.Lock(rd, 5) //5 秒后自动删除Alock
 
-		time.Sleep(7 * time.Second) //等待7秒
-		fmt.Println("111", err)
-		Alock.Unlock(&rd) //想删除的是Alock锁，但是Alock 已经被自动删除 ,Block由于value 不一样，所以也不会删除
-	}()
+        time.Sleep(7 * time.Second) //等待7秒
+        fmt.Println("111", err)
+        Alock.Unlock(rd) //想删除的是Alock锁，但是Alock 已经被自动删除 ,Block由于value 不一样，所以也不会删除
+    }()
 
-	time.Sleep(6 * time.Second) //此时Alock 已经被删除
-	Block := RedisLock{lockKey: "xxxxx"}
-	err := Block.Lock(&rd, 5) //此时 会获取新的lock Block
-	fmt.Println("222", err)
+    time.Sleep(6 * time.Second) //此时Alock 已经被删除
+    Block := RedisLock{lockKey: "xxxxx"}
+    err := Block.Lock(rd, 5) //此时 会获取新的lock Block
+    fmt.Println("222", err)
 
-	time.Sleep(2 * time.Second)
-	Clock := RedisLock{lockKey: "xxxxx"}
-	err = Clock.Lock(&rd, 5) //想获取新的lock Clock，但由于 Block还存在，返回错误
-	fmt.Println("333", err)
+    time.Sleep(2 * time.Second)
+    Clock := RedisLock{lockKey: "xxxxx"}
+    err = Clock.Lock(rd, 5) //想获取新的lock Clock，但由于 Block还存在，返回错误
+    fmt.Println("333", err)
 
-	time.Sleep(10 * time.Second)
+    time.Sleep(2 * time.Second)
 
+    fmt.Println("===================================")
 }
 
 var Redispool *redis.Pool
 
 func init() {
-	Redispool = &redis.Pool{
-		MaxIdle:     10,
-		IdleTimeout: 300 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			tcp := fmt.Sprintf("%s:%d", "127.0.0.1", 32768)
-			c, err := redis.Dial("tcp", tcp)
-			if err != nil {
-				return nil, err
-			}
-			//fmt.Println("connect redis success!")
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
+    Redispool = &redis.Pool{
+        MaxIdle:     10,
+        IdleTimeout: 300 * time.Second,
+        Dial: func() (redis.Conn, error) {
+            tcp := fmt.Sprintf("%s:%d", "47.91.178.4", 9379)
+            c, err := redis.Dial("tcp", tcp)
+            if err != nil {
+                return nil, err
+            }
+            //fmt.Println("connect redis success!")
+            return c, err
+        },
+        TestOnBorrow: func(c redis.Conn, t time.Time) error {
+            _, err := c.Do("PING")
+            return err
+        },
+    }
 
 }
